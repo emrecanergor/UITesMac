fe6dfcfd39616fe4fe4c68eeb6abb77c46cba3b8
ayg-mac <emrecan.ergor@gmail.com>
2018-07-09 15:53:47 +0300

    first

---
+++ UITesMac/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace UITesMac
+{
+ 
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        int sayac = 0;
+        private void onClick(object sender, EventArgs e)
+        {
+            sayac++;
+
+            label1.Text = entry1.Text;
+            buton1.Text = "Degisti";
+
+
+            Button btn = (Button)sender;
+            if (sayac % 2 == 1)
+            {
+                btn.BackgroundColor = Color.Yellow;
+                BackgroundColor = Color.Blue;
+                label1.TextColor = Color.Red;
+
+            }
+            else
+            {
+                btn.BackgroundColor = Color.Purple;
+                BackgroundColor = Color.White;
+                buton1.Text += "R";
+            }
+        }
+    }
+}
---
+++ UI_Test/Tests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace UI_Test
+{
+    //[TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void AppLaunches()
+        {
+            //app.Screenshot("First_Screen");
+            //app.Repl();
+            app.EnterText(c => c.Marked("entry_xyz"), "Deneme1-2-3-1");
+            app.Tap(c => c.Marked("buton_xyz"));
+            app.Tap(c => c.Marked("buton_xyz"));
+
+        }
+    }
+}
